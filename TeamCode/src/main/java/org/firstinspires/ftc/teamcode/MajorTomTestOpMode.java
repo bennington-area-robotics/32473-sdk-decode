b97f64ea2039438dbbbc26ec56bd0662ecafd278
Jack <jackrmallory@proton.me>
2026-03-18 19:31:22 -0400

    does nothing now but better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MajorTomTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MajorTomTestOpMode.java
@@ -2,13 +2,50 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="TestOpModeAndroid")
 public class MajorTomTestOpMode extends LinearOpMode {
+    public int ticks = 0;
+    public ElapsedTime timer = new ElapsedTime();
+
+    public CRServo leftFeeder;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        initialize();
         waitForStart();
-        telemetry.addLine("hello");
+        run();
+        while(opModeIsActive()){
+            tick();
+        }
+    }
+
+    public void initialize() {
+        leftFeeder = hardwareMap.get(CRServo.class, "left_feeder");
+    }
+
+    public void run(){
+
+    }
+
+    public void tick() {
+        ticks++;
+        if (gamepad1.a) {
+            leftFeeder.setPower(1);
+        } else {
+            leftFeeder.setPower(0);
+        }
+        updateTelemetry();
+        timer.reset();
+    }
+
+    public void updateTelemetry(){
+        telemetry.addLine(String.valueOf(timer.milliseconds()));
+        telemetry.addLine("GamepadA " + gamepad1.a);
         telemetry.update();
     }
 }
