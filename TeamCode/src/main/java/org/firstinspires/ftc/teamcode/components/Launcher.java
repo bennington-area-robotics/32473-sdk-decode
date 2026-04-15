04382f29d8f2a2ffa465514c881c7b6a6b067fe9
Jack Mallory <jackrmallory@proton.me>
2026-04-15 18:58:58 -0400

    Added untuned launch PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Launcher.java
@@ -1,32 +1,40 @@
 package org.firstinspires.ftc.teamcode.components;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.core.OpModeCore;
 import org.firstinspires.ftc.teamcode.hardware.SmartMotor;
+import org.firstinspires.ftc.teamcode.hardware.controllers.VelocityPID;
 
+@Configurable
 public class Launcher {
 
     private final ElapsedTime timer = new ElapsedTime();
 
-    private static double TARGET_VELOCITY = 1200;
+    private static double TARGET_VELOCITY = 1000;
     private static double MIN_VELOCITY = TARGET_VELOCITY - 100;
+
+    private final VelocityPID controller;
+
+    public static double kP = 0.0001, kI = 0, kD = 0, kf = 0.001, tolerance = 30;
 
     private final SmartMotor motor;
 
     public Launcher(SmartMotor motor) {
+        this.controller = new VelocityPID.Builder()
+                .setKP(() -> kP)
+                .setKI(() -> kI)
+                .setKD(() -> kD)
+                .setKF(() -> kf)
+                .setTolerance(tolerance)
+                .build();
         this.motor = motor;
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(
-                10,
-                0,
-                5,
-                13
-        )); // TUNE THIS
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        OpModeCore.getTelemetry().addData("Launcher Result", controller::result);
     }
 
     public static void setTargetVelocity(double targetVelocity) {
@@ -49,19 +57,21 @@ public class Launcher {
     }
 
     public void jamPrevention() {
-        double JAM_PREVENT_VELOCITY = 300;
+        double JAM_PREVENT_VELOCITY = 175;
 
-        motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor.setVelocity(JAM_PREVENT_VELOCITY);
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setPower(controller.calcWithVelocity(JAM_PREVENT_VELOCITY, getVelocity()));
     }
 
     public void launch() {
-        motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor.setVelocity(TARGET_VELOCITY);
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor.setPower(controller.calcWithVelocity(TARGET_VELOCITY, getVelocity()));
     }
 
     public double getVelocity() {
-        return motor.getVelocity();
+        float ticksPerDegree = (537.7f / 360f);
+
+        return -motor.getVelocity() / ticksPerDegree;
     }
 
     public boolean atSpeed() {
